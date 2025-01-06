fcd5125e395fe0062ba635335a66c1d04ea8a90a
Emil Bakov <bakovemil@gmail.com>
2025-01-06 15:11:23 +0200

    new one black mode

--- v2/components/sections/Pricing.vue
+++ v2/components/sections/Pricing.vue
@@ -47,7 +47,7 @@
                         </h1>
                         <p class="flex items-center text-gray-600 mb-2">
                             <span
-                                class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0 dark:text-gray-300">
+                                class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0 dark:text-gray-300 dark:text-gray-300">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                     stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                                     <path d="M20 6L9 17l-5-5"></path>
@@ -56,7 +56,7 @@
                         </p>
                         <p class="flex items-center text-gray-600 mb-2">
                             <span
-                                class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0 dark:text-gray-300">
+                                class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0 dark:text-gray-300 dark:text-gray-300">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                     stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                                     <path d="M20 6L9 17l-5-5"></path>
@@ -65,7 +65,7 @@
                         </p>
                         <p class="flex items-center text-gray-600 mb-2">
                             <span
-                                class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0 dark:text-gray-300">
+                                class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0 dark:text-gray-300 dark:text-gray-300">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                     stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                                     <path d="M20 6L9 17l-5-5"></path>
@@ -74,7 +74,7 @@
                         </p>
                         <p class="flex items-center text-gray-600 mb-6">
                             <span
-                                class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0 dark:text-gray-300">
+                                class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0 dark:text-gray-300 dark:text-gray-300">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                     stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                                     <path d="M20 6L9 17l-5-5"></path>
